45a4a58dda791721a8c8e4ff1f7e371cc70506f6
usman-197 <malik@analog.one>
2022-10-31 18:49:20 +0500

    nodemailer

---
+++ src/main.js
@@ -0,0 +1,30 @@
+
+require('dotenv').config()
+const nodemailer = require("nodemailer");
+
+(async function run() {
+    console.log('test');
+
+    const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true, 
+        auth: {
+          user: process.env.MAIL_USER_EMAIL,
+          pass: process.env.MAIL_USER_PASS
+        },
+      });
+    await transporter.sendMail({
+        from: process.env.USER_FROM_EMAIL, // sender address
+        to: process.env.USER_TO_EMAIL, // list of receivers
+        subject: `
+            Result
+        `,
+        text: `
+            Cron Result
+        `,
+        html: `
+            <h1> R.E.S.U.L.T. </h1>
+        `,
+      });
+})();
